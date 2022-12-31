077301f226bd227dbae3c61ea65174491281eba6
jinlee057 <jinlee057@gmail.com>
2022-12-31 19:20:00 +0900

    update all
    upload task

---
+++ task/work.js
@@ -0,0 +1,56 @@
+/*
+1. 요구사항 확인하기
+2. 요구사항 바탕으로 인풋 생각하기
+3. 인풋 바탕으로 단계별 한글로 함수 설계하기
+4. 단계별 구글링 키워드 생각하기
+*/
+
+/* Assignment 1
+1. 요구사항 : 페이지 로딩 시 form태그 안의 p태그의 글씨 변경
+2. 인풋,아웃풋 : page load, innerHTML
+3. 함수설계 : body.onload => $textbox = #display form p.innerHTML 변경
+4. 키워드 : js이용 p태그 글씨 변경. 페이지 로드 시 이벤트 실행
+*/
+
+/* Assignment 2
+1. 요구사항 : 선택한 탭의 배경과 글자색 변경
+2. 인풋,아웃풋 : DIV 클릭, DIV backgroudcolor, color 변경
+3. 함수설계 : DOM API로 할 경우 다섯개의 탭을 각각 구분해야하나..?
+    클릭 시 발생할 이벤트를 함수로 만든 후 div태그에 각각 설정.
+    함수츨 호출한 대상을 설정 (this)
+    function tabAttrChange(this)
+    this.backgroundcolor change
+    this.color change
+    this제외한 나머지 div배경, 폰트 색 변경
+4. 키워드 : 같은 class내 다른 div의 배경색, 폰트색 변경
+*/
+
+/* Assignment 3
+1. 요구사항 : 클릭한 div내의 innerHTML과 같은 div만 출력
+2. 인풋,아웃풋 : div #class 클릭=> (this)의 innerHTML, 
+    div #block의 innerHTML의 앞에서 2글자
+    
+3. 함수설계 :
+4. 키워드 :
+*/
+
+/* Assignment 4
+1. 요구사항 :
+2. 인풋,아웃풋 :
+3. 함수설계 :
+4. 키워드 :
+*/
+
+/* Assignment 5
+1. 요구사항 :
+2. 인풋,아웃풋 :
+3. 함수설계 :
+4. 키워드 :
+*/
+
+/* Assignment 6
+1. 요구사항 :
+2. 인풋,아웃풋 :
+3. 함수설계 :
+4. 키워드 :
+*/
