2526ef2894c5e7f694a0ca8f613339f14d258c37
jinlee057 <jinlee057@gmail.com>
2023-01-01 21:32:51 +0900

    level2.js
    level2.js done

--- quizz/level2/level2.js
+++ quizz/level2/level2.js
@@ -24,33 +24,48 @@ import { RESERVATION_LIST } from './reservation .js';
     $name && $phone : false면 에러문 3 리턴
 
     두 가지 모두 만족 하면 RESERVATION_LIST number 리턴
-에러문
-1. 이름이 없을 때 '존재하지 않는 이름입니다'
-2. 전화번호가 없을 떄 '없는 번호 입니다'
-3. 이름과 전화번호가 일치하지 않을 때 '예약자명과 번호가 일치하지 않습니다'
+        에러문
+        1. 이름이 없을 때 '존재하지 않는 이름입니다'
+        2. 전화번호가 없을 떄 '없는 번호 입니다'
+        3. 이름과 전화번호가 일치하지 않을 때 '예약자명과 번호가 일치하지 않습니다'
 
-4. input name dom api 가져오기
+4. input name dom api 가져오기, 객체가 있는 배열에서 특정값 찾기
 */
 
-// const $userName = document.getElementsByName("user-name") //return nodelist whats it? and whats difference with querySelector?
-const $userName2 = document.querySelector("[name='user-name']") //equivalent with form>p
-// const $userName3 = document.querySelector("form>p:first-child>input") // form p:first or form>p:first what's distinction in between?
+// const $userName = document.getElementsByName("user-name") //nodelist로 반환? 쿼리셀렉터와 무슨차이?
+const $userName2 = document.querySelector("[name='user-name']") //3번과 같은 결과값 반환, 사용목적?
+// const $userName3 = document.querySelector("form>p:first-child>input") // form p:first or form>p:first 둘의 차이는?
 const $phoneNo = document.querySelector("[name='user-phone']")
 const $resultList = document.querySelector("#reservation-number")
 const $chkBtn = document.querySelector("form >button")
 
-$chkBtn.addEventListener('click',resevationChk)
+$chkBtn.addEventListener('click',resevationChk) //.onclick & .addEventListner 둘의 차이 및 사용하기 위해서는?
 
-function resevationChk(e) { //why put e here?
-    e.preventDefault(); //any other case not using this one?
-    
-    
 
-    const result = $userName2.value + $phoneNo.value
+
+function resevationChk(e) { //e 들어가는이유?
+    e.preventDefault(); //이걸 안쓰는 경우?
     
-    $resultList.innerHTML = result;
+        const resName = RESERVATION_LIST.filter(function(el){
+            return el.name === $userName2.value;
+        });
+        const resPhone = RESERVATION_LIST.filter(function(el){
+            return el.phone === $phoneNo.value;
+        });
+        const result = RESERVATION_LIST.filter(function(el){
+            return el.name === $userName2.value && el.phone===$phoneNo.value 
+        });
+
+        if(resName == "") return $resultList.innerHTML = errChk[101]
+        if(resPhone == "") return $resultList.innerHTML = errChk[102]
+        if(result=="") return $resultList.innerHTML = errChk[103]
+        $resultList.innerHTML = result[0].number;
+        
 }
 
-const errChk() {
+const errChk = {
+    101 : "존재하지 않는 이름입니다",
+    102 : "없는 번호입니다",
+    103 : "일치하는 내역이 없습니다"
 
-}
+};
