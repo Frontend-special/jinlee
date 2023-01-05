76bcc18e4d405e84a552678027e0a6be6d23d7e8
jinlee057 <jinlee057@gmail.com>
2023-01-05 21:02:05 +0900

    level4.js updated
    comment added

--- task/answer/level4/level4.js
+++ task/answer/level4/level4.js
@@ -10,7 +10,7 @@ let totalPage = 80;
 let PAGE_LST = [];
 let currentPageIndx = 0;
 let currentPage = new URLSearchParams(location.search).get('page') || 1; //|| 1 존재이유
-console.log(currentPage)
+
 Array.prototype.division = function (div) {
   const arr = this;
   const result = [];
@@ -21,14 +21,17 @@ Array.prototype.division = function (div) {
   return result;
 };
 
-(function pagaNation(totalPage, currentPage) {
+(function pagaNation(tpage, cpage) {
   const paageNation_list = [];
   const paageNation_limit = 10;
-  currentPageIndx = Math.ceil(currentPage / paageNation_limit) - 1;
-  for (let i = 1; i <= totalPage; i++) {
+  currentPageIndx = Math.ceil(cpage / paageNation_limit) -1;
+  // console.log(cpage, paageNation_limit, cpage / paageNation_limit, Math.ceil(cpage / paageNation_limit))
+  for (let i = 1; i <= tpage; i++) {
     paageNation_list.push(i);
   }
   PAGE_LST = paageNation_list.division(paageNation_limit);
+  
+  // console.log(PAGE_LST, paageNation_list)
   renderPageNation();
 })(totalPage, currentPage);
 
@@ -81,16 +84,27 @@ slice통해 만들어진 rssult 배열에 추가 하고 result리턴
 */
 
 /* pagaNation(totalPage, currentPage)
-즉시실행함수
+즉시실행함수 (function 함수명(전달받은인자)(인수전달))
 input : totalPage, currentPage (페이지 전체 갯수, url주소 상 page의 값 = 현재 페이지 위치?)
 빈 배열 생성, 한 페이지에 표시될 페이지 갯수 리밋은 10
+
 총페이지수 = math.ceil(전체페이지수/한화면에서 보여줄 크기) => math.ceil 올림 80/10 = 8 총 8페이지
-
-for 1~totalpage(80)RKwl 1씩 증가하면서 paageNation_list에 푸쉬
-
-
+cpage=73, p_limit=10, cpage/p_limit = 7.3 => roundup = 8 but -1 =7
+1~10 : 0page
+11~20 : 1page...
+71~80 : 7page
+----만약 p_limit = 5?
+1/5=0.2 roundup 1 -1 = 0
+15/5 = 3 roundup 4 -1 = 3
+-----------------------------현재 페이지가 속해야할 구간으르 구한다
+ 
+for 1~totalpage(80)까지 1씩 증가하면서 paageNation_list에 푸쉬
+limit수만큼 배열을 묶음 -> 배열? 객체?
+renderpagenation 실행
 */
 
-
-
 //구글 검색 javascript 전체페이지 묶음으로 나누기 https://lts0606.tistory.com/477
+
+/*
+
+*/
