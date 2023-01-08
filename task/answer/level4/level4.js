9fa79585b6b1960755bd34ac94c19a598b43c48a
jinlee057 <jinlee057@gmail.com>
2023-01-08 13:11:11 +0900

    level4.js
    New comment added in level4.js

--- task/answer/level4/level4.js
+++ task/answer/level4/level4.js
@@ -103,7 +103,6 @@ limit수만큼 프로토타입 division 메소드 이용 배열을 묶음 -> 배
 renderpagenation 실행
 */
 
-//구글 검색 javascript 전체페이지 묶음으로 나누기 https://lts0606.tistory.com/477
 
 /* renderPageNation
 currentPageIndex통해서 구간 반환 후 가로리스트에 넣는데...
@@ -116,3 +115,66 @@ currentPageIndex통해서 구간 반환 후 가로리스트에 넣는데...
 현재페이지 인덱스 ++ 렌더 실행
 
 */
+
+
+/*구글링 키워드
+ javascript 전체페이지 묶음으로 나누기 https://lts0606.tistory.com/477
+  js 전체페이지 원하는 갯수만큼 페이지 나누기
+  js 페이지 10개단위로 이동
+  js 앞,뒤 페이지 이동하면서 주소에 페이지번호 반환
+ */
+
+
+
+
+  /*
+  1. 요구 사항 
+    전체 페이지 갯수를 원하는 갯수의 그룹으로 묶고,
+    그룹 내 해당하는 페이지 오픈 시 그 그룹만 페이지 리스트에 노출.
+    < >   버튼 클릭시 앞/뒤 페이지 간 이동
+    << >> 버튼 클릭시 페이지 그룹 간 이동
+
+  2. 인풋/아웃풋
+
+    필요 요소들
+    totalpage = 전체페이지 갯수
+    chunksize = 그룹으로 묶고자 하는 페이지 갯수
+    indexgroup = 청크사이즈만큼 묶인 리스트 그룹 (0: 1-1#, 1: 1#+1 - 2#)
+    pagelist = 화면에 표시될 페이지리스트 번호 클릭시 해당페이지로 이동
+    <>버튼 = 클릭 시 페이지 앞뒤로 이동가능한 버튼
+    #page = 번호 클릭시 오픈될 각각 페이지
+
+    필요 이벤트
+    pagelist내 번호 클릭, <> 버튼 클릭
+
+    input
+    totalpage, chunksize, currentpage, event,
+    
+    output
+    indexgroup (index# | 리스트내에 있을 번호들의 집합
+                    0 | (i#*chunksize) + 1 - (i#+1)*chunksize / if chunksize = 5, 1 - 5
+                    1 | (i#*chunksize) + 1 - (i#+1)*chunksize / if chunksize = 5, 6 - 10
+    pagelist 1 2 3 4 5 ... / 6 7 8 ...
+    #page  해당 번호에 맞춰 오픈될 웹페이지
+
+  3. 한글 함수 & 구글키워드
+    함수 1. 페이지그룹구하기 (전체페이지 갯수 원하는 사이즈로 그릅화)
+          주어진 토탈페이지 갯수와 청크사이즈로 인덱스 번호별 페이지 그룹 구하기
+          input : totalpage, chunksize / indexgroup [[],[],[]]
+    
+    함수 2. 선택된 페이지의 위치를 확인하는 함수 (현재 페이지의 주소 확인하는 방법)
+          현재 열려있는 페이지의 인덱스 그룹 확인
+          input : currentpage / output : 해당페이지의 indexgroup[]
+    
+    함수 3. 페이지리스트생성 (숫자로된 리스트 생성하면서 페이지별 이동)
+          해당 페이지의 인덱스 그룹만 화면에 노출.
+          페이지 번호에 a태그 생성. 해당 번호의 웹페이지로 이동
+          input : indexgroup / output : pagelist with a tag, #page
+
+    함수 4. 버튼 이벤트 (버튼 클릭시 페이지 앞뒤로 이동, 시작과 끝 이동)
+          버튼 클릭시 페이지 번호 앞뒤로 이동
+          단, 페이지그룹의 양 끝단 클릭시 페이지 그룹도 이동
+          input : buttonevent / output : pagelist with a tag, #page
+  
+
+  */
